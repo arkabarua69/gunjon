9e9606a20bcbb284d009c57c8f07fb1229bd764f
GUNJON BARUA ARKA <155982830+arkabarua69@users.noreply.github.com>
2024-03-02 09:05:10 -0600

    arka
    arka

---
+++ main.js
@@ -0,0 +1,12 @@
+//  HOME 
+
+
+
+// Testimonial swiper
+var swiper = new Swiper(".testimonial-wrapper", {
+loop: 'true',
+pagination:{
+    el:".swiper-pagination",
+    clickable: true,
+    },
+})
